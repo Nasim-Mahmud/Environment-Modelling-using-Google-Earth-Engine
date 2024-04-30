df6ca6e4aa0a7fd9d26f56bdcf002715d83b7a74
Nasim Mamud Mishu <nasimmahmud1301@gmail.com>
2024-04-30 11:45:49 +0600

    Updated earth engine single roi(Dhaka).js

--- earth engine  single roi(Dhaka).js
+++ earth engine  single roi(Dhaka).js
@@ -1,16 +1,17 @@
-var image = ee.Image(ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
-    .filterBounds(regionOfInterest)
-    .filterDate('2015-05-01', '2019-01-30')
+var image = ee.Image(ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
+    .filterBounds(roi)
+    .filterDate('2019-05-01', '2023-01-30')
     .sort('CLOUD_COVER')
     .first());
-Map.addLayer(image, {bands: ['B4', 'B3', 'B2'],min:0, max: 3000}, 'image');
+Map.addLayer(image, {bands: ['SR_B4', 'SR_B3', 'SR_B2'],min:0, max: 3000}, 'image');
+
 
 //Merge feature collection
 var newfc = UrbanAreas.merge(WaterLands).merge(GreenAreas);
 print(newfc);
 
 //Create training data
-var bands = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7'];
+var bands = ['SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7'];
 var training = image.select(bands).sampleRegions({
   collection: newfc,
   properties: ['landcover'],
@@ -31,11 +32,9 @@ var classified = image.select(bands).classify(classifier);
 
 //Display classification
 Map.centerObject(newfc, 11);
-Map.addLayer(image,{bands: ['B4', 'B3', 'B2'], max:3},'Landsat image')
+Map.addLayer(image,{bands: ['SR_B4', 'SR_B3', 'SR_B2'], max:3},'Landsat image')
 Map.addLayer(classified,
 {min: 0, max: 2, palette: ['000000', '3fbfe1', '12900a']},
 'classification');
 
 Map.addLayer(newfc);
-
-
